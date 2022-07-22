ecdaa734487820c0a592dee9634676871c788cc0
Oluwasegun <80678596+NewthingAde@users.noreply.github.com>
2022-07-22 22:17:07 +0200

    complete project file

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = '';
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+const apiId = 'vo0z3kaxxh';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
 
 export const authConfig = {
-  domain: '',
-  clientId: '',
+  domain: 'dev-fjp-3wf9.us.auth0.com',
+  clientId: 'b4bG4TV8cDkqvafjImlMeEE4qUctL7Ti',
   callbackUrl: 'http://localhost:3000/callback'
 };
